0e9407cf7d8c2ae0bfb7e8b506c49b162a86b5e5
Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>
2021-12-10 15:53:25 +0700

    bizflyctl 0.2.1 (new formula)

---
+++ Formula/bizflyctl.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Bizflyctl < Formula
+  desc "Command Line for BizFly Cloud"
+  homepage ""
+  url "https://github.com/bizflycloud/bizflyctl/archive/refs/tags/v0.2.1.zip"
+  sha256 "bcc6df619ec50170684aa71bbfeb8add5aaa5115a5d268e78998d450ca4c1507"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bizflyctl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
